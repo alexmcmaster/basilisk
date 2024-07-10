4e51df14135d1cb0f1a01f1363e1c7e664875c9c
Alex McMaster <mcmaster2010@gmail.com>
2024-07-10 09:30:28 -0700

    sim1-CSS: add 6 CSS units

--- sims/sim1-CSS.py
+++ sims/sim1-CSS.py
@@ -13,7 +13,7 @@ from Basilisk import __path__
 bskPath = __path__[0]
 fileName = os.path.basename(os.path.splitext(__file__)[0])
 
-from Basilisk.simulation import spacecraft
+from Basilisk.simulation import spacecraft, coarseSunSensor
 from Basilisk.utilities import SimulationBaseClass, macros,\
     orbitalMotion, simIncludeGravBody, unitTestSupport, vizSupport
 from Basilisk.simulation import simSynch
@@ -82,6 +82,46 @@ if __name__ == "__main__":
                                    [2.*macros.D2R],
                                    [3.*macros.D2R]]  # angular rates (rad/s)
 
+    sunPositionMsgData = messaging.SpicePlanetStateMsgPayload()
+    sunPositionMsgData.PositionVector = [0.0, orbitalMotion.AU*1000.0, 0.0]
+    sunPositionMsg = messaging.SpicePlanetStateMsg().write(sunPositionMsgData)
+    eclipseMsgData = messaging.EclipseMsgPayload()
+    eclipseMsgData.shadowFactor = 0.5
+    eclipseMsg = messaging.EclipseMsg().write(eclipseMsgData)
+
+    # Define CSS units. Parameters common to all are hardcoded, while parameters
+    # that differ between units are given as parameters.
+    def setupCSS(CSS, tag, pos, direction):
+        CSS.fov = 80. * macros.D2R
+        CSS.scaleFactor = 2.0
+        CSS.maxOutput = 2.0
+        CSS.minOutput = 0.5
+        CSS.kellyFactor = 0.2
+        CSS.sunInMsg.subscribeTo(sunPositionMsg)
+        CSS.stateInMsg.subscribeTo(scObject.scStateOutMsg)
+        CSS.sunEclipseInMsg.subscribeTo(eclipseMsg)
+        CSS.ModelTag = tag
+        CSS.r_B = np.array(pos)
+        CSS.nHat_B = np.array(direction)
+
+    CSS0 = coarseSunSensor.CoarseSunSensor()
+    setupCSS(CSS0, "CSS0_sensor", [0, 0, 0], [1, 0, 0])
+    CSS1 = coarseSunSensor.CoarseSunSensor()
+    setupCSS(CSS1, "CSS1_sensor", [0, 0, 0], [-1, 0, 0])
+    CSS2 = coarseSunSensor.CoarseSunSensor()
+    setupCSS(CSS2, "CSS2_sensor", [0, 0, 0], [0, 1, 0])
+    CSS3 = coarseSunSensor.CoarseSunSensor()
+    setupCSS(CSS3, "CSS3_sensor", [0, 0, 0], [0, -1, 0])
+    CSS4 = coarseSunSensor.CoarseSunSensor()
+    setupCSS(CSS4, "CSS4_sensor", [0, 0, 0], [0, 0, 1])
+    CSS5 = coarseSunSensor.CoarseSunSensor()
+    setupCSS(CSS5, "CSS5_sensor", [0, 0, 0], [0, 0, -1])
+    cssList = [CSS0, CSS1, CSS2, CSS3, CSS4, CSS5]
+
+    # Add CSS units to sim
+    for css in cssList:
+        scSim.AddModelToTask(simTaskName, css)
+
     # Logging
     numDataPoints = 400
     samplingTime = unitTestSupport.samplingTime(simulationTime,
@@ -89,13 +129,21 @@ if __name__ == "__main__":
                                                 numDataPoints)
     dataLog = scObject.scStateOutMsg.recorder(samplingTime)
     scSim.AddModelToTask(simTaskName, dataLog)
+    cssLogs = [css.cssDataOutMsg.recorder() for css in cssList]
+    for cssl in cssLogs:
+        scSim.AddModelToTask(simTaskName, cssl)
 
     # Final setup
     clockSync = simSynch.ClockSynch()
     clockSync.accelFactor = 1.0
     scSim.AddModelToTask(simTaskName, clockSync)
-    vizSupport.enableUnityVisualization(scSim, simTaskName, scObject,
-                                        liveStream=True)
+    viz = vizSupport.enableUnityVisualization(scSim, simTaskName, scObject,
+                                              liveStream=True,
+                                              cssList=[cssList])
+    vizSupport.setInstrumentGuiSetting(viz, viewCSSPanel=True,
+                                       viewCSSCoverage=True,
+                                       viewCSSBoresight=True,
+                                       showCSSLabels=True)
     scSim.InitializeSimulation()
     scSim.ConfigureStopTime(simulationTime)
     scSim.SetProgressBar(True)
@@ -109,8 +157,10 @@ if __name__ == "__main__":
     # Plot results
     posData = dataLog.r_BN_N
     velData = dataLog.v_BN_N
+    cssData = [cssl.OutputData for cssl in cssLogs]
     np.set_printoptions(precision=16)
     plt.close("all")  # clears out plots from earlier test runs
+
     plt.figure(1)
     fig = plt.gcf()
     ax = fig.gca()
@@ -122,9 +172,7 @@ if __name__ == "__main__":
     plt.legend(loc='lower right')
     plt.xlabel('Time [orbits]')
     plt.ylabel('Inertial Position [km]')
-    figureList = {}
-    pltName = fileName + "1" + "LEO"
-    figureList[pltName] = plt.figure(1)
+
     plt.figure(2)
     fig = plt.gcf()
     ax = fig.gca()
@@ -136,7 +184,17 @@ if __name__ == "__main__":
     plt.plot(posData[:, 0] * macros.NANO2SEC / P, smaData, color='#aa0000')
     plt.xlabel('Time [orbits]')
     plt.ylabel('SMA [km]')
-    pltName = fileName + "2" + "LEO"
-    figureList[pltName] = plt.figure(2)
+
+    plt.figure(3)
+    fig = plt.gcf()
+    ax = fig.gca()
+    ax.ticklabel_format(useOffset=False, style='plain')
+    for i, cssl in enumerate(cssLogs):
+        plt.plot(cssl.times() * macros.NANO2SEC / P, cssl.OutputData,
+                 label=f"CSS{i}")
+    plt.legend()
+    plt.xlabel('Time [orbits]')
+    plt.ylabel('CSS readings')
+
     plt.show()
     plt.close("all")
